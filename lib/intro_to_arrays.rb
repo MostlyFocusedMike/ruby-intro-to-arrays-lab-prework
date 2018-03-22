4129615e0af410ae83c6899daf9304c6855149ea
Mike C <mikecronin92@gmail.com>
2018-03-22 03:19:45 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -2,3 +2,18 @@ def instantiate_new_array
   return []
 end
 
+def array_with_two_elements
+  return [1,2]
+end 
+
+def first_element(arr)
+  return arr[0]
+end
+
+def third_element(arr)
+  return arr[2]
+end
+
+def third_element(arr)
+  return arr[-1]
+end
